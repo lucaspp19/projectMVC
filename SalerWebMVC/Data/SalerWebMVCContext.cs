231b07f73b32c16b9655cc41f19cb0a793a6ce76
lucaspp19 <lucaspp191@gmail.com>
2022-11-04 12:04:51 -0300

    CRUD Scaffolding

---
+++ SalerWebMVC/Data/SalerWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalerWebMVC.Models;
+
+namespace SalerWebMVC.Data
+{
+    public class SalerWebMVCContext : DbContext
+    {
+        public SalerWebMVCContext (DbContextOptions<SalerWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalerWebMVC.Models.Department> Department { get; set; }
+    }
+}
